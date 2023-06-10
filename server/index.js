6f6336899b248ee45cefe8d6f11d2d65fbae05b1
bydhrma <bayudharmap@gmail.com>
2023-06-10 13:40:01 +0800

    create api routes

--- server/index.js
+++ server/index.js
@@ -2,6 +2,8 @@ import express from 'express';
 import * as dotenv from 'dotenv';
 import cors from 'cors';
 import connectDB from './mongodb/connect.js';
+import dalleRoutes from './routes/dall-eRoute.js';
+import postRoutes from './routes/postRoute.js';
 
 dotenv.config();
 
@@ -10,6 +12,9 @@ app.use(cors());
 app.use(express.json({
   limit: '50mb'
 }));
+
+app.use('api/v1/post', postRoutes);
+app.use('api/v1/dalle', dalleRoutes);
 
 app.get('/', async(req, res) => {
   res.send('Hello,This works');
